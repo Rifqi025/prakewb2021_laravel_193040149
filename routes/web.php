2c59e1cdb37b42516cf4ba18b3dc439cd00c5b0a
Rifqi025 <muliawan027@gmail.com>
2021-10-15 22:40:39 +0700

    menyelesaikan video ke 5 - Model, Collection  & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,78 +33,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rifqi Muliawan",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Fugiat soluta omnis error adipisci consectetur earum, esse
-            odit quam sunt tempore accusamus beatae in? Officia natus autem, 
-            cum praesentium quod et explicabo, deserunt debitis nisi sapiente earum aliquid, 
-            voluptatibus sed quasi odio tenetur excepturi ab quas illo. Sint corporis nisi accusantium ad 
-            rerum consequatur vel necessitatibus obcaecati omnis quam, atque error nemo non,
-            perferendis cumque vitae dolorem labore sequi soluta blanditiis iure. 
-            Dicta quidem laborum ea nulla accusantium culpa animi perferendis."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Kevin Fredrian Syach",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam ut atque obcaecati consequuntur, 
-            corrupti reprehenderit eveniet corporis. Corrupti eveniet, nesciunt error vitae, suscipit quas adipisci, 
-            distinctio fugiat sunt consectetur ratione ut voluptatum eaque tempora. Numquam delectus vitae doloremque reprehenderit. 
-            Quas accusamus aut soluta officia totam illo itaque, eos quasi eveniet! Qui totam amet alias repudiandae quibusdam odio cumque, 
-            ipsam inventore deleniti magnam saepe rem delectus eos ad exercitationem architecto quasi facere soluta. 
-            Eos, ipsa exercitationem molestiae cum maxime amet nisi eaque quae! Ducimus explicabo a corrupti mollitia. 
-            Velit maiores commodi temporibus quae totam vitae tempore architecto. Illum cumque laborum voluptatum."
-        ],
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rifqi Muliawan",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Fugiat soluta omnis error adipisci consectetur earum, esse
-            odit quam sunt tempore accusamus beatae in? Officia natus autem, 
-            cum praesentium quod et explicabo, deserunt debitis nisi sapiente earum aliquid, 
-            voluptatibus sed quasi odio tenetur excepturi ab quas illo. Sint corporis nisi accusantium ad 
-            rerum consequatur vel necessitatibus obcaecati omnis quam, atque error nemo non,
-            perferendis cumque vitae dolorem labore sequi soluta blanditiis iure. 
-            Dicta quidem laborum ea nulla accusantium culpa animi perferendis."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Kevin Fredrian Syach",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam ut atque obcaecati consequuntur, 
-            corrupti reprehenderit eveniet corporis. Corrupti eveniet, nesciunt error vitae, suscipit quas adipisci, 
-            distinctio fugiat sunt consectetur ratione ut voluptatum eaque tempora. Numquam delectus vitae doloremque reprehenderit. 
-            Quas accusamus aut soluta officia totam illo itaque, eos quasi eveniet! Qui totam amet alias repudiandae quibusdam odio cumque, 
-            ipsam inventore deleniti magnam saepe rem delectus eos ad exercitationem architecto quasi facere soluta. 
-            Eos, ipsa exercitationem molestiae cum maxime amet nisi eaque quae! Ducimus explicabo a corrupti mollitia. 
-            Velit maiores commodi temporibus quae totam vitae tempore architecto. Illum cumque laborum voluptatum."
-        ],
-    ];
-
-    $new_post =[];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{slug}', [PostController::class, 'show']);
