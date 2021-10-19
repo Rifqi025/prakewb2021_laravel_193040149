87e37806639c39343088f81a7191ae242b44b842
Rifqi025 <muliawan027@gmail.com>
2021-10-19 15:54:17 +0700

    menyelesaikan video ke 6 - Database, Migration dan Eloquent

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-        });
-    }
+        });    }
 
     /**
      * Reverse the migrations.
