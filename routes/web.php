39c7350196db0f49d5c44514604984f11f2af9ef
Rifqi025 <muliawan027@gmail.com>
2021-11-10 23:14:45 +0700

    Menyelesaikan video ke 13 - Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,18 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
-        "name" => "Rifqi Muliawan",
-        "email" => "muliawan027@gmail.com",
-        "image" => "profile.jpeg"
+        'title' => 'About',
+        'active' => 'about',
+        'name' => 'Rifqi Muliawan',
+        'email' => 'muliawan027@gmail.com',
+        'image' => 'profile.jpeg'
     ]);
 });
 
@@ -47,17 +49,18 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'title' => "Post By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
