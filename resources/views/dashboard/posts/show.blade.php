5ace1f5dfa81520b4c9a62fb5025c1d4d64090a6
Rifqi025 <muliawan027@gmail.com>
2021-12-04 16:20:37 +0700

    Menyelesaikan video ke 20 - Update & Delete Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,10 +8,14 @@
  
           <a href="/dashboard/posts" class="btn btn-success mb-3"><span data-feather="arrow-left"></span>
           Back to all my posts</a>
-          <a href="" class="btn btn-warning mb-3"><span data-feather="edit"></span>
+          <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning mb-3"><span data-feather="edit"></span>
           Edit</a>
-          <a href="" class="btn btn-danger mb-3"><span data-feather="x-circle"></span>
-          Delete</a>
+          <form actton="/dashboard/posts/{{ $post->slug }}" method="post" Class="d-inline">
+          @method( 'delete')
+          @csrf
+          <button class="btn btn-danger mb-3" onclick="return confirm( 'Are you sure?')">
+          <span data-feather="x-circle"></span> Delete</button>
+          </form>
  
           <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
  
